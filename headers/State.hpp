4784bbd5ac3bdbb7cd162c5babe9f8b2a44b8a2c
tbl4h <mic.maci2891@gmail.com>
2022-02-03 18:24:29 +0100

    Building GameFSM

--- headers/State.hpp
+++ headers/State.hpp
@@ -11,111 +11,114 @@
 
 using namespace std;
 
-enum class State
+class GameFSM
 {
-    Start,
-    InLocation,
-    Buy,
-    Sell,
-    Quit
-};
-inline ostream &operator<<(ostream &os, const State &s)
-{
-    switch (s)
+    enum class EnumState
     {
-    case State::Start:
-        os << "Start." << endl;
-        break;
-    case State::InLocation:
-        os << "Gracz znajduje się w lokacji." << endl;
-        break;
-    case State::Buy:
-        os << "Kup narkotyk: " << endl;
-        break;
-    case State::Sell:
-        os << "Sprzedaj narkotyk: " << endl;
-        break;
-    case State::Quit:
-        os << "Koniec gry" << endl;
-        break;
-    }
-    return os;
-}
-enum class Trigger
-{
-    StartGame,
-    ToLocation,
-    ToBuy,
-    ToSell,
-    EndGame,
-};
-inline ostream &operator<<(ostream &os, const Trigger &s)
-{
-    switch (s)
+        Start,
+        InLocation,
+        Buy,
+        Sell,
+        Quit
+    };
+    friend ostream &operator<<(ostream &os, const EnumState &s)
     {
-    case Trigger::StartGame:
-        os << "Gra się zaczyna." << endl;
-        break;
-    case Trigger::ToLocation:
-        os << "Przemieszczasz się do lokacji." << endl;
-        break;
-    case Trigger::ToBuy:
-        os << "Co zamierzasz kupić." << endl;
-        break;
-    case Trigger::ToSell:
-        os << "Co zamierzasz sprzedać." << endl;
-        break;
-    case Trigger::EndGame:
-        os << "Gra się kończy." << endl;
-        break;
-    }
-    return os;
-}
-class GameFST
-{
-    map<State, vector<pair<Trigger, State>>> rules;
-    State current_state {State::Start};
-    State exitState {State::Quit};
-    
-    void init_rules(){
-        rules[State::Start] = {
-            {Trigger::ToLocation,State::InLocation}
-        };
-        rules[State::InLocation] = {
-            {Trigger::ToLocation,State::InLocation},
-            {Trigger::ToBuy,State::Buy},
-            {Trigger::ToSell,State::Sell},
-            {Trigger::EndGame,State::Quit}
-        };
-        rules[State::Buy] = {
-            {Trigger::ToLocation,State::InLocation},            
-        };
-        rules[State::Sell] = {
-            {Trigger::ToLocation,State::InLocation}
-        };
-    }
-    public:
-        GameFST(){};
-        void play(){
-            while(true){
-                cout << current_state;
-                select_trigger:
-                cout << "Wybierz akcję: " << endl;
-                int i =0;
-                for (auto item : rules[current_state])
-                {
-                    cout << i++ << item.first << endl;
-                }
-                int input;
-                cin >> input;
-                if(input < 0 || (input + 1) > rules[current_state].size())
-                {
-                    cout << "Nieprawidłowa opcja. Spróbuj jeszce raz." << endl;
-                    goto select_trigger;
-                }
-
-                current_state = rules[current_state][input].second;
-                if(current_state == exitState) break;
-            }
+        switch (s)
+        {
+        case EnumState::Start:
+            os << "Start." << endl;
+            break;
+        case EnumState::InLocation:
+            os << "Gracz znajduje się w lokacji." << endl;
+            break;
+        case EnumState::Buy:
+            os << "Kup narkotyk: " << endl;
+            break;
+        case EnumState::Sell:
+            os << "Sprzedaj narkotyk: " << endl;
+            break;
+        case EnumState::Quit:
+            os << "Koniec gry" << endl;
+            break;
         }
+        return os;
+    }
+    enum class EnumTrigger
+    {
+        StartGame,
+        ToLocation,
+        ToBuy,
+        ToSell,
+        EndGame,
+    };
+    friend ostream &operator<<(ostream &os, const EnumTrigger &s)
+    {
+        switch (s)
+        {
+        case EnumTrigger::StartGame:
+            os << "Gra się zaczyna." << endl;
+            break;
+        case EnumTrigger::ToLocation:
+            os << "Przemieszczasz się do lokacji." << endl;
+            break;
+        case EnumTrigger::ToBuy:
+            os << "Co zamierzasz kupić." << endl;
+            break;
+        case EnumTrigger::ToSell:
+            os << "Co zamierzasz sprzedać." << endl;
+            break;
+        case EnumTrigger::EndGame:
+            os << "Gra się kończy." << endl;
+            break;
+        }
+        return os;
+    }
+    map<EnumState, vector<pair<EnumTrigger, EnumState>>> rules;
+    EnumState current_state{EnumState::Start};
+    EnumState exitEnumState{EnumState::Quit};
+
+    void init_rules()
+    {
+        rules[EnumState::Start] = {
+            {EnumTrigger::ToLocation, EnumState::InLocation}};
+        rules[EnumState::InLocation] = {
+            {EnumTrigger::ToLocation, EnumState::InLocation},
+            {EnumTrigger::ToBuy, EnumState::Buy},
+            {EnumTrigger::ToSell, EnumState::Sell},
+            {EnumTrigger::EndGame, EnumState::Quit}};
+        rules[EnumState::Buy] = {
+            {EnumTrigger::ToLocation, EnumState::InLocation},
+        };
+        rules[EnumState::Sell] = {
+            {EnumTrigger::ToLocation, EnumState::InLocation}};
+    }
+
+public:
+    GameFSM(){};
+
+    void play()
+    {
+        while (true)
+        {
+            cout << current_state;
+        select_trigger:
+            cout << "Wybierz akcję: " << endl;
+            int i = 0;
+            for (auto item : rules[current_state])
+            {
+                cout << i++ << item.first << endl;
+            }
+            int input;
+            cin >> input;
+            if (input < 0 || (input + 1) > rules[current_state].size())
+            {
+                cout << "Nieprawidłowa opcja. Spróbuj jeszce raz." << endl;
+                goto select_trigger;
+            }
+
+            current_state = rules[current_state][input].second;
+            if (current_state == exitEnumState)
+                break;
+        }
+    }
 };
